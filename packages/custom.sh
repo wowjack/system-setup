1146954ea780434432574122992a5a7942b9c5b8
Jack Kingham <wowknead@gmail.com>
2026-01-11 14:30:59 -0700

    Started implementing distro specific behavior.

---
+++ packages/custom.sh
@@ -0,0 +1,7 @@
+# Install other software that shouldn't be installed via flatpak or apt/dnf/pacman
+#   vscode
+#   rust
+#   node
+#   go
+#   java
+#   python
--- utils.sh
+++ utils.sh
@@ -40,3 +40,52 @@ error_handler() {
     exit "$exit_code"
 }
 trap 'error_handler $? $LINENO "$BASH_COMMAND"' ERR
+
+
+# Request password once and keep auth cred alive until the script exits
+sudo -v
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+
+detect_pkg_manager() {
+    if [[ -f /etc/os-release ]]; then
+        source /etc/os-release
+    else
+        log INFO "/etc/os-release does not exist. Distro unknown."
+        ID="unknown"
+    fi
+
+    case "$ID" in
+        debian|ubuntu) PKG_MANAGER="apt" ;;
+        fedora) PKG_MANAGER="dnf" ;;
+        arch) PKG_MANAGER="pacman" ;;
+        unknown|*)
+            log INFO "Distro unknown: $ID. Detecting package manager via command_exists fallback."
+            if command_exists apt; then
+                PKG_MANAGER="apt"
+            elif command_exists dnf; then
+                PKG_MANAGER="dnf"
+            elif command_exists pacman; then
+                PKG_MANAGER="pacman"
+            else
+                log ERROR "No supported package manager found."
+                exit 1
+            fi
+            ;;
+    esac
+
+    log INFO "Detected distro: $ID (package manager: $PKG_MANAGER)"
+}
+
+
+pkg_update() {
+    case "$PKG_MANAGER" install
+        apt) apt update ;;
+        dnf) dnf check-update || true ;;
+        pacman) pacman -Su --noconfirm ;;
+    esac
+}
