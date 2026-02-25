09fddfbae685278d547777940e20c781fb13f8f1
Jack Kingham <wowknead@gmail.com>
2026-02-24 22:48:49 -0700

    Added vscode, extension_manager flatpaks

--- installers/vscode.py
+++
@@ -1,9 +0,0 @@
-from util import download_file, install_package
-from pathlib import Path
-
-VSCODE_SRC = "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-VSCODE_DST = Path(__file__).resolve().parent / "code.deb"
-
-def install():
-    download_file(VSCODE_SRC, VSCODE_DST)
-    install_package(f"{VSCODE_DST}")
